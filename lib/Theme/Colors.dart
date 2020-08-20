373ecbef63e6a92a55b7da5e5f04b83b43880256
CCE NOTES <jacobranjit000@gmail.com>
2020-08-20 19:51:05 +0530

    +Added flutter dependencies in pubspec.yaml file

---
+++ lib/Theme/Colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+
+
+  final background = const Color(0x26a69a);
+  final colorPrimary = const Color(0x00766c);
+  final colorPrimaryDark = const Color(0x005b4f);
+  final colorAccent = const Color(0x14ffec);
+  final textColor = const Color(0xffffff);
+  final downloadbuttoncolor = const Color(0x64d8cb);
